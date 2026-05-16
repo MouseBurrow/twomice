3267113e1fd9b54b7db2e309c8a5bffbabaddf82
Sylviromi <sylviromi.ss@proton.me>
2026-05-16 23:42:15 +0300

    Extract libs into independent git repo
    libs/ (config, custom_headers, easy_errors) is now its own git repo
    with filtered history. Service Cargo.toml files reference it via
    git dependencies with subdir paths.
    
    To push: git@github.com/twomice/twomice-libs.git

--- libs/config/src/lib.rs
+++
@@ -1,4 +0,0 @@
-pub mod app_data;
-pub mod app_envs;
-pub mod config;
-pub mod logger;
--- libs/config/src/logger.rs
+++
@@ -1,5 +0,0 @@
-use env_logger::Env;
-
-pub fn init() {
-    env_logger::init_from_env(Env::default().default_filter_or("debug"));
-}
